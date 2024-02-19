1c124945600b19595cb25f7d879ed506a6f72dba
TinNguyen <ductin16111@gmail.com>
2024-02-19 21:58:33 +0200

    update test for product reducer

--- src/redux/product/productSlice.ts
+++ src/redux/product/productSlice.ts
@@ -21,21 +21,20 @@ interface ProductState {
 const initialState: ProductState = {
 	allProducts: [],
 	loading: false,
-    error: null,
+	error: null,
 };
 
 const url = "https://api.escuelajs.co/api/v1/products";
 
 export const fetchAllProductsAsync = createAsyncThunk(
 	"fetchAllProductsAsync",
-	async () => {
+	async (_, { rejectWithValue }) => {
 		try {
 			const res = await axios(url);
 			const data = res.data;
 			return data;
 		} catch (e) {
-			const error = e as Error;
-			return error;
+			return rejectWithValue(e);
 		}
 	}
 );
@@ -47,25 +46,21 @@ const productSlice = createSlice({
 	extraReducers(builder) {
 		// fetch all products
 		builder.addCase(fetchAllProductsAsync.fulfilled, (state, action) => {
-			if (!(action.payload instanceof Error)) {
-				state.allProducts = action.payload; 
-                state.loading = false;
-			}
+			state.allProducts = action.payload;
+			state.loading = false;
 		});
 		builder.addCase(fetchAllProductsAsync.pending, (state) => {
 			state.loading = true;
 		});
-        builder.addCase(fetchAllProductsAsync.rejected, (state, action) => {
-            if (action.payload instanceof Error) {
-                state.loading = false;
-                state.error = action.payload.message;
-            }
-        })
+		builder.addCase(fetchAllProductsAsync.rejected, (state, action) => {
+			state.loading = false;
+			state.error = action.error.message ?? "error";
+		});
 	},
 });
 
-export const { } = productSlice.actions
+export const {} = productSlice.actions;
 
-export const selectProducts = (state: RootState) => state.products.allProducts
+export const selectProducts = (state: RootState) => state.products.allProducts;
 
-export default productSlice.reducer
+export default productSlice.reducer;
--- src/test/productReducer.test.ts
+++ src/test/productReducer.test.ts
@@ -1,13 +1,90 @@
-import reducer, { ProductType } from "../redux/product/productSlice";
+import reducer, {
+	ProductType,
+	fetchAllProductsAsync,
+} from "../redux/product/productSlice";
 
 describe("product reducer", () => {
+	// mock data
+	const mockProducts: ProductType[] = [
+		{
+			id: 1,
+			title: "product1",
+			price: 1,
+			description: "product1",
+			images: ["img1", "img2"],
+			category: {
+				id: 1,
+				name: "category 1",
+				image: "img1",
+			},
+		},
+		{
+			id: 2,
+			title: "product2",
+			price: 2,
+			description: "product2",
+			images: ["img1", "img2"],
+			category: {
+				id: 2,
+				name: "category 2",
+				image: "img2",
+			},
+		},
+	];
+	// initial state
+	const initialState = {
+		allProducts: [],
+		loading: false,
+		error: null,
+	};
+	/*********************************************************************** */
+
+	// test initial state
 	test("should return initial state", () => {
-		const initialState = {
-			allProducts: [],
+		const expected = initialState;
+		const received = reducer(undefined, { type: "" });
+		expect(received).toEqual(expected);
+	});
+
+	// test fulfull
+	test("should return a list of products", () => {
+		const received = reducer(
+			initialState,
+			fetchAllProductsAsync.fulfilled(mockProducts, "fulfilled")
+		);
+		const expected = {
+			allProducts: mockProducts,
 			loading: false,
 			error: null,
 		};
-		const state = reducer(undefined, { type: "" });
-		expect(state).toEqual(initialState);
+		expect(received).toEqual(expected);
+	});
+
+	// test pending
+	test("should have loading truthy when fetch is pending", () => {
+		const state = reducer(
+			initialState,
+			fetchAllProductsAsync.pending("pending")
+		);
+		expect(state).toEqual({
+			allProducts: [],
+			loading: true,
+			error: null,
+		});
+	});
+
+	// test reject
+
+	test("should have error", () => {
+		const error = new Error("error");
+		const state = reducer(
+			initialState,
+			fetchAllProductsAsync.rejected(error, "error")
+		);
+		expect(state).toEqual({
+			allProducts: [],
+			loading: false,
+			error: error.message,
+		});
 	});
 });
