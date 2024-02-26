b1603faef35faa690e032f52611d8cef8b3a35a6
TinNguyen <ductin16111@gmail.com>
2024-02-26 23:35:17 +0200

    add drop down menu in nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { fetchAllCategoriesAsync } from "../redux/category/categorySlice";
 
 const NavBar: React.FC = () => {
+	const [dropDown, setDropDown] = useState("");
 	const categories = useAppSelector(
 		(state) => state.categories.allCategories
 	);
@@ -17,28 +18,67 @@ const NavBar: React.FC = () => {
 	if (categories.length > 0) {
 		for (let i = 0; i < 4; i++) {
 			categoriesShort.push(
-				<Link to={`/category/${categories[i].id}`}>
-					<span>{categories[i].name}</span>
+				<Link
+					to={`/category/${categories[i].id}`}
+					key={categories[i].id}
+				>
+					<div
+						className="navbar-category"
+						onMouseOver={() => setDropDown(categories[i].name)}
+						onMouseLeave={() => setDropDown("")}
+					>
+						<span>{categories[i].name}</span>
+					</div>
 				</Link>
 			);
 		}
 	}
 
+	const dropDownClothes = (
+		<table
+			onMouseOver={() => setDropDown("Clothes")}
+			onMouseLeave={() => setDropDown("")}
+		>
+			<tbody>
+				<tr>
+					<th>Highlights</th>
+					<th>Gender</th>
+					<th>Ready to wear</th>
+				</tr>
+				<tr>
+					<td>New arrivals</td>
+					<td>Men</td>
+					<td>Coats and outerwear</td>
+				</tr>
+				<tr>
+					<td>Classic uniform</td>
+					<td>Women</td>
+					<td>Sport coats and tailoring</td>
+				</tr>
+			</tbody>
+		</table>
+	);
+
 	return (
-		<nav>
-			<section className="navbar-left">
-				{categoriesShort.map((category) => category)}
+		<>
+			<nav>
+				<section className="navbar-left">
+					{categoriesShort.map((category) => category)}
+				</section>
+				<section className="navbar-center">E-commerce</section>
+				<section className="navbar-right">
+					<span>Stores</span>
+					<span>About</span>
+					<span>Log in</span>
+					<Link to={"./cart"}>
+						<span>Cart</span>
+					</Link>
+				</section>
+			</nav>
+			<section className="drop-down">
+				{dropDown === "Clothes" ? dropDownClothes : null}
 			</section>
-			<section className="navbar-center">E-commerce</section>
-			<section className="navbar-right">
-				<span>Stores</span>
-				<span>About</span>
-				<span>Log in</span>
-				<Link to={"./cart"}>
-					<span>Cart</span>
-				</Link>
-			</section>
-		</nav>
+		</>
 	);
 };
 
