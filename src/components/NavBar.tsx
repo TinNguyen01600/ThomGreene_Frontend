eaadefd1d59cb0908977abae70df59e9509f4672
TinNguyen <ductin16111@gmail.com>
2024-02-26 03:23:21 +0200

    update scss styling for nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,6 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { fetchAllCategoriesAsync } from "../redux/category/categorySlice";
 
@@ -10,9 +12,16 @@ const NavBar: React.FC = () => {
 	useEffect(() => {
 		dispatch(fetchAllCategoriesAsync());
 	});
+
 	const categoriesShort = [];
-	for (let i = 0; i < 4; i++) {
-		categoriesShort.push(<span>{categories[i].name}</span>);
+	if (categories.length > 0) {
+		for (let i = 0; i < 4; i++) {
+			categoriesShort.push(
+				<Link to={`/category/${categories[i].id}`}>
+					<span>{categories[i].name}</span>
+				</Link>
+			);
+		}
 	}
 
 	return (
@@ -20,15 +29,15 @@ const NavBar: React.FC = () => {
 			<section className="navbar-left">
 				{categoriesShort.map((category) => category)}
 			</section>
-			<section className="navbar-center">
-                E-commerce
-            </section>
+			<section className="navbar-center">E-commerce</section>
 			<section className="navbar-right">
-                <span>Stores</span>
-                <span>About</span>
-                <span>Log in</span>
-                <span>Cart</span>
-            </section>
+				<span>Stores</span>
+				<span>About</span>
+				<span>Log in</span>
+				<Link to={"./cart"}>
+					<span>Cart</span>
+				</Link>
+			</section>
 		</nav>
 	);
 };
