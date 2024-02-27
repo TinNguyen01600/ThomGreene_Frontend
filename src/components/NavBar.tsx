22d07b1cf4f3b8814f05a64e26d6887ee9774798
TinNguyen <ductin16111@gmail.com>
2024-02-27 11:48:25 +0200

    add more category drop downs and scss

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -34,6 +34,8 @@ const NavBar: React.FC = () => {
 		}
 	}
 
+	/*********************************************************************************************** */
+
 	const dropDownClothes = (
 		<table
 			onMouseOver={() => setDropDown("Clothes")}
@@ -58,6 +60,100 @@ const NavBar: React.FC = () => {
 			</tbody>
 		</table>
 	);
+	const dropDownElectronics = (
+		<table
+			onMouseOver={() => setDropDown("Electronics")}
+			onMouseLeave={() => setDropDown("")}
+		>
+			<tbody>
+				<tr>
+					<th>Gaming</th>
+					<th>Mobile Devices</th>
+					<th>Computer and office supplies</th>
+				</tr>
+				<tr>
+					<td>Gaming Laptops</td>
+					<td>Tablets</td>
+					<td>Printers</td>
+				</tr>
+				<tr>
+					<td>Gaming Keyboards</td>
+					<td>Smart watches and Sport watches</td>
+					<td>Routers and network devices</td>
+				</tr>
+				<tr>
+					<td>Consoles and Accessories</td>
+					<td>E-book reading devices</td>
+					<td></td>
+				</tr>
+			</tbody>
+		</table>
+	);
+	const dropDownFurniture = (
+		<table
+			onMouseOver={() => setDropDown("Furniture")}
+			onMouseLeave={() => setDropDown("")}
+		>
+			<tbody>
+				<tr>
+					<th>Furniture</th>
+					<th>Textiles</th>
+					<th>Cooking and dining</th>
+                    <th>Decorations</th>
+				</tr>
+				<tr>
+					<td>Armchairs and Divans</td>
+					<td>Curtains and Roller blinds</td>
+					<td>Plates and bowls</td>
+					<td>Candles and Wicks</td>
+				</tr>
+				<tr>
+					<td>Bookshelves</td>
+					<td>Carpets</td>
+					<td>Baking supplies</td>
+					<td>Mirrors</td>
+				</tr>
+				<tr>
+					<td>Sofas</td>
+					<td>Blankets</td>
+					<td></td>
+					<td>Room scents</td>
+				</tr>
+                <tr>
+					<td></td>
+					<td>Seat cushions</td>
+					<td></td>
+					<td></td>
+				</tr>
+			</tbody>
+		</table>
+	);
+    const dropDownShoes = (
+		<table
+			onMouseOver={() => setDropDown("Shoes")}
+			onMouseLeave={() => setDropDown("")}
+		>
+			<tbody>
+				<tr>
+					<td>Loafers</td>
+					<td>Brogues</td>
+					<td>Heels</td>
+				</tr>
+				<tr>
+					<td>Boots</td>
+					<td>Slippers and Sandals</td>
+					<td>Sneakers</td>
+				</tr>
+                <tr>
+					<td>Training and Gym</td>
+					<td>Track and Field</td>
+					<td></td>
+				</tr>
+			</tbody>
+		</table>
+	);
+
+	/*********************************************************************************************** */
 
 	return (
 		<>
@@ -76,7 +172,15 @@ const NavBar: React.FC = () => {
 				</section>
 			</nav>
 			<section className="drop-down">
-				{dropDown === "Clothes" ? dropDownClothes : null}
+				{dropDown === "Clothes"
+					? dropDownClothes
+					: dropDown === "Electronics"
+					? dropDownElectronics
+					: dropDown === "Furniture"
+					? dropDownFurniture
+					: dropDown === "Shoes"
+					? dropDownShoes
+					: null}
 			</section>
 		</>
 	);
